346b5a2ea23ff2ec6cd6e70a81d6b5434a5d0a70
AlphJose <alphonsajose906@gmail.com>
2022-01-13 15:15:31 +0530

    making async db

--- app/core/database.py
+++ app/core/database.py
@@ -1,13 +1,38 @@
-from sqlalchemy import create_engine
+import asyncio
+
+# from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import async_scoped_session, AsyncSession, create_async_engine
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from asyncio import current_task
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:p4ssw0rd@127.0.0.1:3306/todoapp"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://root:p4ssw0rd@127.0.0.1:3306/shoppingcartapp"
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=True
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# loop = asyncio.get_event_loop()
+# yield loop
+# loop.close()
 Base = declarative_base()
+
+# SessionLocal = async_scoped_session(
+#     sessionmaker(
+#         autocommit=False,
+#         autoflush=True,
+#         bind=engine,
+#         class_=AsyncSession,
+#     ),
+#     scopefunc=current_task,
+# )
+
+SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+# We should disable the "expire on commit" behavior of sessions with expire_on_commit=False.
+# This is because in async settings, we don't want SQLAlchemy to issue new SQL queries to
+# the database when accessing already committed objects.
 
--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.services import user_service
+from app.models import user_info
+from app.core.database import engine
 
 app = FastAPI()
+
+user_info.Base.metadata.create_all(bind=engine)
+
+app.include_router(user_service.router)
 
 
 @app.get("/")
